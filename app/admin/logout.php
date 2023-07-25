e19ed0e422299704b7b98b261caebc1e8c48cad2
zikrullah5 <107057397+zikrullah5@users.noreply.github.com>
2023-07-25 13:59:53 +0700

    first commit

---
+++ app/admin/logout.php
@@ -0,0 +1,11 @@
+<?php
+//------------------------------::::::::::::::::::::------------------------------\\
+//------------------------------::::::::::::::::::::------------------------------\\
+session_start();
+unset($_SESSION['id_user']);
+unset($_SESSION['fullname']);
+unset($_SESSION['username']);
+unset($_SESSION['status']);
+
+$_SESSION['berhasil_keluar'] = "Anda telah berhasil keluar !!";
+header("location: ../../masuk");
---
+++ app/user/pages/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.0
+    </div>
+    <strong>&copy; <?= date('Y'); ?>. By. </strong> ZZ5.| Repost by <a href='https://sharecodingan.blogspot.com/' title='sharecodingan.blogspot.com' target='_blank'>Dzikri.</a>
+    
+</footer>
